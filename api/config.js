20af410c293e7ad296645ef3fc77a520a17c221f
Shubham Sharma <shubhamjee183@gmail.com>
2026-02-14 19:47:42 +0530

    fixed issue for google sign in

---
+++ api/config.js
@@ -0,0 +1,18 @@
+module.exports = (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method !== 'GET') {
+    return res.status(405).json({ error: 'Method not allowed' });
+  }
+
+  return res.status(200).json({
+    googleClientId: process.env.GOOGLE_CLIENT_ID || '',
+  });
+};
